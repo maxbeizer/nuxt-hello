4a16ff0f64f6d17644e9485eb40de478f3179e38
max beizer <max.beizer@gmail.com>
2019-02-13 20:47:36 -0600

    Initial commit

---
+++ pages/about/index.vue
@@ -0,0 +1,19 @@
+<template>
+  <section class="container">
+    <div>
+      <h1 class="title">OHAI ABOUT</h1>
+      <div class="links">
+        <a href="https://nuxtjs.org/" target="_blank" class="button--green">Documentation</a>
+        <a href="https://github.com/nuxt/nuxt.js" target="_blank" class="button--grey">GitHub</a>
+        <nuxt-link to="/about" class="button--green">About</nuxt-link>
+      </div>
+    </div>
+  </section>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style scoped>
+</style>
